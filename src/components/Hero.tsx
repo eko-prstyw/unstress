8fc1aea4c74ca39a0718b28a24c020d6b8f809c4
eko-prstyw <ekoprasetyo103@gmail.com>
2023-11-10 23:38:50 +0800

    login page and register page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,10 +17,15 @@ export default function Hero() {
               Hilangkan Stres
             </div>
             <div className="font-robotoSlab mb-8 text-justify text-neutral">
-              Selamat datang di tempat yang tepat untuk meraih keseimbangan emosional dan mental dalam hidup Anda, kami akan membantu Anda menemukan kedamaian dan kebahagiaan di tengah tantangan dan tekanan yang mungkin Anda hadapi.
+              Selamat datang di tempat yang tepat untuk meraih keseimbangan
+              emosional dan mental dalam hidup Anda, kami akan membantu Anda
+              menemukan kedamaian dan kebahagiaan di tengah tantangan dan
+              tekanan yang mungkin Anda hadapi.
             </div>
             <div className="flex gap-6 mb-8">
-              <button className="w-56 h-11 font-roboto bg-primary rounded-xl text-white">Mulai Jelajahi UnStress</button>
+              <button className="w-56 h-11 font-roboto bg-primary rounded-xl text-white">
+                Mulai Jelajahi UnStress
+              </button>
             </div>
             <div className="flex gap-6 mb-8 text-neutral">
               <div className="flex gap-1 items-center">
@@ -37,20 +42,36 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <Image src={HeroImage1} alt="Gambar Hero" width={600} height={500}></Image>
+          <Image
+            src={HeroImage1}
+            alt="Gambar Hero"
+            width={600}
+            height={500}
+          ></Image>
         </div>
       </div>
       <div className="max-w-full bg-[#F2F2F2]">
         <div className="flex justify-between px-[100px] pt-20">
-          <Image src={HeroImage2} alt="Gambar Hero" width={500} height={500}></Image>
+          <Image
+            src={HeroImage2}
+            alt="Gambar Hero"
+            width={500}
+            height={500}
+          ></Image>
           <div className="grid content-center w-[550px]">
-            <p className="font-roboto text-[#A09A9A] mb-3">Home | Tentang Kami</p>
+            <p className="font-roboto text-[#A09A9A] mb-3">
+              Home | Tentang Kami
+            </p>
             <div className="font-bold text-6xl font-roboto mb-3">UnStress</div>
             <div className="font-robotoSlab text-justify mb-6">
-              Selamat datang di situs "Mengatasi Stres." Kami adalah platform online yang didedikasikan untuk membantu Anda memahami, mengelola, dan mengatasi stres dalam kehidupan sehari-hari. Kami percaya bahwa stres adalah tantangan
-              universal yang...
+              Selamat datang di situs "Mengatasi Stres." Kami adalah platform
+              online yang didedikasikan untuk membantu Anda memahami, mengelola,
+              dan mengatasi stres dalam kehidupan sehari-hari. Kami percaya
+              bahwa stres adalah tantangan universal yang...
             </div>
-            <button className="text-white bg-primary py-2 px-6 rounded-xl font-roboto justify-self-start">Lihat Semua</button>
+            <button className="text-white bg-primary py-2 px-6 rounded-xl font-roboto justify-self-start">
+              Lihat Semua
+            </button>
           </div>
         </div>
       </div>
